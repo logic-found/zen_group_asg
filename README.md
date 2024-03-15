77170fe01f580f2236ac09b3e4472237c45289a6
rashikasahu112 <rashikasahu162@gmail.com>
2024-03-15 22:06:49 +0530

    changes

---
+++ README.md
@@ -0,0 +1,5 @@
+### Setup
+#### update the db connection string in server .env file
+### CMD
+#### Frontend : npm run dev
+#### Backend : npm start
--- Server/index.js
+++ Server/index.js
@@ -7,6 +7,7 @@ const Invoice = require('./routes/Invoice')
 require('dotenv').config()
 const PORT = process.env.PORT || 8000
 
+
 app.use(cors())
 app.use(express.json())
 
